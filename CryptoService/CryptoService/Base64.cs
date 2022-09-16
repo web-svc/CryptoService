06fb07648a9cc19bb6383921606f55fd39e1bcde
Narender Kumar <Narender.Kumar@multichoice.co.za>
2022-09-16 22:49:09 +0530

    Update

---
+++ CryptoService/CryptoService/Base64.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptoService
+{
+    using CryptoService.Interface;
+    public class Base64: IAction
+    {
+        readonly IBase64 Algorithm = new Algorithms();
+        
+        public string Encrypt(ICipherInput cipherInput) => Algorithm.Encrypt(cipherInput.CipherText);
+
+        public string Decrypt(ICipherInput cipherInput) => Algorithm.Decrypt(cipherInput.CipherText);
+    }
+}
